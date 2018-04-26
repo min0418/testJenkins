22a2f38486cc9ca628ad9944d6d4576ea10f4c36
leo <min0418@gmail.com>
2018-04-26 16:58:13 +0900

    return string has changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String index(){
-        return "Hello world    99";
+        return "Hello world  ddd";
     }
 
     public static void main(String[] args) {
